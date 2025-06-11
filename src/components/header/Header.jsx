2e57b2d2979ea8ea656678c1839fa3c848161890
Muratbek Nassyrov <130246057+wordpress-developer01@users.noreply.github.com>
2025-06-11 21:28:22 +0300

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,12 +4,12 @@ import "./header.css";
 function Header({ darkMode, toggleTheme }) {
     return (
         <div className="Header-body">
-            <img className="Logo1" src="/assets/images/logo.svg" alt="Logo" />
+            <img className="Logo1" src="assets/images/logo.svg" alt="Logo" />
 
             <button className="theme-toggle" onClick={toggleTheme}>
                 <img
                     className="Logo2"
-                    src={darkMode ? "/assets/images/icon-sun.svg" : "/assets/images/icon-moon.svg"}
+                    src={darkMode ? "assets/images/icon-sun.svg" : "assets/images/icon-moon.svg"}
                     alt="Theme toggle"
                 />
             </button>
